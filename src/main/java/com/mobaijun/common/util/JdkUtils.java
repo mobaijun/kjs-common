8b360a3e96cba70fc3d82f8fc4c543ef1e7b9287
mobaijun <mobaijun8@163.com>
2022-06-29 15:26:03 +0800

    @modify :art: update JdkUtils

--- src/main/java/com/mobaijun/common/util/JdkUtils.java
+++ src/main/java/com/mobaijun/common/util/JdkUtils.java
@@ -53,4 +53,137 @@ public class JdkUtils {
     public static int getServerCpuNum() {
         return Runtime.getRuntime().availableProcessors();
     }
+
+    /**
+     * 获取Java 供应商的 URL
+     */
+    public static String getVendorUrl() {
+        return System.getProperty(JdkConstant.VENDOR_URL);
+    }
+
+    /**
+     * 获取Java 虚拟机规范版本
+     */
+    public static String getVmSpecificationVersion() {
+        return System.getProperty(JdkConstant.VM_SPECIFICATION_VERSION);
+    }
+
+    /**
+     * 获取Java 虚拟机规范供应商
+     */
+    public static String getVmSpecificationName() {
+        return System.getProperty(JdkConstant.VM_SPECIFICATION_NAME);
+    }
+
+    /**
+     * 获取Java 运行时环境规范版本
+     */
+    public static String getVmVersion() {
+        return System.getProperty(JdkConstant.VM_VERSION);
+    }
+
+    /**
+     * 获取Java 运行时环境规范供应商
+     */
+    public static String getSpecificationVendor() {
+        return System.getProperty(JdkConstant.SPECIFICATION_VENDOR);
+    }
+
+    /**
+     * 获取Java 运行时环境规范名称
+     */
+    public static String getSpecificationName() {
+        return System.getProperty(JdkConstant.SPECIFICATION_NAME);
+    }
+
+    /**
+     * 获取Java 类格式版本号
+     */
+    public static String getClassVersion() {
+        return System.getProperty(JdkConstant.CLASS_VERSION);
+    }
+
+    /**
+     * 获取Java 类路径
+     */
+    public static String getClassPath() {
+        return System.getProperty(JdkConstant.CLASS_PATH);
+    }
+
+    /**
+     * 获取加载库时搜索的路径列表
+     */
+    public static String getLibraryPath() {
+        return System.getProperty(JdkConstant.LIBRARY_PATH);
+    }
+
+    /**
+     * 获取默认的临时文件路径
+     */
+    public static String getTmpdir() {
+        return System.getProperty(JdkConstant.TMPDIR);
+    }
+
+    /**
+     * 获取要使用的 JIT 编译器的名称
+     */
+    public static String getCompiler() {
+        return System.getProperty(JdkConstant.COMPILER);
+    }
+
+    /**
+     * 获取一个或多个扩展目录的路径
+     */
+    public static String getExtDirs() {
+        return System.getProperty(JdkConstant.EXT_DIRS);
+    }
+
+    /**
+     * 获取操作系统的版本
+     */
+    public static String getOsVersion() {
+        return System.getProperty(JdkConstant.OS_VERSION);
+    }
+
+    /**
+     * 获取文件分隔符（在 UNIX 系统中是“/”）
+     */
+    public static String getSileSeparator() {
+        return System.getProperty(JdkConstant.FILE_SEPARATOR);
+    }
+
+    /**
+     * 获取路径分隔符（在 UNIX 系统中是“:”）
+     */
+    public static String getPathSeparator() {
+        return System.getProperty(JdkConstant.PATH_SEPARATOR);
+    }
+
+    /**
+     * 获取行分隔符（在 UNIX 系统中是“/n”）
+     */
+    public static String getLineSeparator() {
+        return System.getProperty(JdkConstant.LINE_SEPARATOR);
+    }
+
+    /**
+     * 获取用户的账户名称
+     */
+    public static String getUserName() {
+        return System.getProperty(JdkConstant.USER_NAME);
+    }
+
+    /**
+     * 获取用户的主目录
+     */
+    public static String getUserHome() {
+        return System.getProperty(JdkConstant.USER_HOME);
+    }
+
+    /**
+     * 获取用户的当前工作目录
+     */
+    public static String getUserDir() {
+        return System.getProperty(JdkConstant.USER_DIR);
+    }
 }
--- src/main/java/com/mobaijun/common/util/constant/JdkConstant.java
+++ src/main/java/com/mobaijun/common/util/constant/JdkConstant.java
@@ -24,17 +24,17 @@ public class JdkConstant {
     public static final String VM_NAME = "java.vm.name";
 
     /**
-     * 获取主机
+     * Java 安装目录
      */
     public static final String HOME = "java.home";
 
     /**
-     * 64位JDK：amd64 ，32位JDK：x86
+     * 操作系统的架构
      */
     public static final String OS_ARCH = "os.arch";
 
     /**
-     * 获取主机名
+     * 操作系统的名称
      */
     public static final String OS_NAME = "os.name";
 
@@ -42,4 +42,104 @@ public class JdkConstant {
      * 64位JDK：64 ，32位JDK：32
      */
     public static final String ARCH_DATA_MODEL = "sun.arch.data.model";
+
+    /**
+     * Java 运行时环境供应商
+     */
+    public static final String VENDOR = "java.vendor";
+
+    /**
+     * Java 供应商的 URL
+     */
+    public static final String VENDOR_URL = "java.vendor.url";
+
+    /**
+     * Java 虚拟机规范版本
+     */
+    public static final String VM_SPECIFICATION_VERSION = "java.vm.specification.version";
+
+    /**
+     * Java 虚拟机规范供应商
+     */
+    public static final String VM_SPECIFICATION_NAME = "java.vm.specification.name";
+
+    /**
+     * Java 运行时环境规范版本
+     */
+    public static final String VM_VERSION = "java.vm.version";
+
+    /**
+     * Java 运行时环境规范供应商
+     */
+    public static final String SPECIFICATION_VENDOR = "java.specification.vendor";
+
+    /**
+     * Java 运行时环境规范名称
+     */
+    public static final String SPECIFICATION_NAME = "java.specification.name";
+
+    /**
+     * Java 类格式版本号
+     */
+    public static final String CLASS_VERSION = "java.class.version";
+
+    /**
+     * Java 类路径
+     */
+    public static final String CLASS_PATH = "java.class.path";
+
+    /**
+     * 加载库时搜索的路径列表
+     */
+    public static final String LIBRARY_PATH = "java.library.path";
+
+    /**
+     * 默认的临时文件路径
+     */
+    public static final String TMPDIR = "java.io.tmpdir";
+
+    /**
+     * 要使用的 JIT 编译器的名称
+     */
+    public static final String COMPILER = "java.compiler";
+
+    /**
+     * 一个或多个扩展目录的路径
+     */
+    public static final String EXT_DIRS = "java.ext.dirs";
+
+    /**
+     * 操作系统的版本
+     */
+    public static final String OS_VERSION = "os.version";
+
+    /**
+     * 文件分隔符（在 UNIX 系统中是“/”）
+     */
+    public static final String FILE_SEPARATOR = "file.separator";
+
+    /**
+     * 路径分隔符（在 UNIX 系统中是“:”）
+     */
+    public static final String PATH_SEPARATOR = "path.separator";
+
+    /**
+     * 行分隔符（在 UNIX 系统中是“/n”）
+     */
+    public static final String LINE_SEPARATOR = "line.separator";
+
+    /**
+     * 用户的账户名称
+     */
+    public static final String USER_NAME = "user.name";
+
+    /**
+     * 用户的主目录
+     */
+    public static final String USER_HOME = "user.home";
+
+    /**
+     * 用户的当前工作目录
+     */
+    public static final String USER_DIR = "user.dir";
 }
