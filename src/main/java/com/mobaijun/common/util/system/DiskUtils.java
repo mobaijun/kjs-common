4d6bc0a4dbe9d1d8da329bb3d88220fbddd78152
mobaijun <mobaijun8@163.com>
2022-06-29 16:13:58 +0800

    @modify :art: update getMemoryInfo

--- src/main/java/com/mobaijun/common/util/system/DiskUtils.java
+++ src/main/java/com/mobaijun/common/util/system/DiskUtils.java
@@ -1,10 +1,14 @@
 package com.mobaijun.common.util.system;
 
 import com.mobaijun.common.util.constant.NumberConstant;
+import com.sun.management.OperatingSystemMXBean;
 
 import java.io.File;
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * software：IntelliJ IDEA 2022.1
@@ -24,5 +28,25 @@ public class DiskUtils {
             return null;
         }
         return new ArrayList<>(Arrays.asList(disks));
+    }
+
+    /**
+     * @return 获取内存使用情况
+     */
+    public static Map<String, String> getMemoryInfo() {
+        Map<String, String> map = new ConcurrentHashMap<>(1);
+        OperatingSystemMXBean mem = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        // 获取内存总容量
+        map.put("totalMemorySize", transformation(mem.getTotalPhysicalMemorySize()));
+        // 获取可用内存容量
+        map.put("freeMemorySize", transformation(mem.getFreePhysicalMemorySize()));
+        return map;
+    }
+
+    /**
+     * 将字节容量转化为GB
+     */
+    public static String transformation(long size) {
+        return size / 1024 / 1024 / 1024 + "GB";
     }
 }
