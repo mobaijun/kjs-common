38dbfcbe97e430fabef00cd4c7def2dcf781adf5
mobaijun <mobaijun8@163.com>
2022-07-19 16:15:02 +0800

    @modify :sparkles: update MoBaiJun

--- src/main/java/com/mobaijun/common/MoBaiJun.java
+++ src/main/java/com/mobaijun/common/MoBaiJun.java
@@ -24,22 +24,22 @@ import java.util.Set;
 /**
  * software：IntelliJ IDEA 2022.1
  * class name: MoBaiJun
- * class description：<img src="https://www.ghibli.jp/gallery/redturtle049.jpg" alt="migrate" height="400" width="800">
+ * class description：<img src="https://w.wallhaven.cc/full/l3/wallhaven-l315vy.png" alt="migrate" height="400" width="800">
  *
  * @author MoBaiJun 2022/7/13 16:15
  */
 public class MoBaiJun {
 
-    private static Set<Class<?>> getAllUtils() {
-        return ClassUtil.scanPackage("com.mobaijun.common",
-                (clazz) -> (!clazz.isInterface()) && StrUtil.endWith(clazz.getSimpleName(), "Utils"));
+    private static Set<Class<?>> getAllUtils(String packageName, String suffix) {
+        return ClassUtil.scanPackage(packageName,
+                (clazz) -> (!clazz.isInterface()) && StrUtil.endWith(clazz.getSimpleName(), suffix));
     }
 
     /**
      * 控制台打印所有工具类
      */
-    public static void printAllUtils() {
-        final Set<Class<?>> allUtils = getAllUtils();
+    public static void printAllUtils(String packageName, String suffix) {
+        final Set<Class<?>> allUtils = getAllUtils(packageName, suffix);
         final ConsoleTable consoleTable = ConsoleTable.create().addHeader("工具类名", "所在包");
         for (Class<?> clazz : allUtils) {
             consoleTable.addBody(clazz.getSimpleName(), clazz.getPackage().getName());
