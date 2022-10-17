9849dc8fb41ed56bf1f952049cb465c1773449c6
mobaijun <mobaijun8@163.com>
2022-10-17 08:46:29 +0800

    :art: 添加代替if和switch的方法

---
+++ src/main/java/com/mobaijun/common/function/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * software：IntelliJ IDEA 2022.1
+ * package name: com.mobaijun.common.function.impl
+ * package description： lambda 实现
+ *
+ * @author MoBaiJun 2022/9/29 8:43
+ */
+package com.mobaijun.common.function.impl;
