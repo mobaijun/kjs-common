5ceb8d0d3685def3a6ec13a6ad6d866f5a0a7efa
mobaijun <mobaijun8@163.com>
2022-11-14 17:46:40 +0800

    :art: update StreamUtils.java

--- src/main/java/com/mobaijun/common/util/stream/StreamUtils.java
+++ src/main/java/com/mobaijun/common/util/stream/StreamUtils.java
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -99,6 +100,20 @@ public class StreamUtils {
             return null;
         }
         return data.stream().distinct().collect(Collectors.toList());
+    }
+
+    /**
+     * 根据主键去重
+     *
+     * @param keyExtractor key
+     * @param <T>          类型
+     * @return 去重后的值
+     */
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>(10);
+        // putIfAbsent方法添加键值对，如果map集合中没有该key对应的值，则直接添加，并返回null，如果已经存在对应的值，则依旧为原来的值。
+        // 如果返回null表示添加数据成功(不重复)，不重复(null==null :TRUE)
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
 
     /**
