8f2f01b40790ee161a85b05ffdc924bc64eb2b58
mobaijun <mobaijun8@163.com>
2022-12-09 16:28:32 +0800

    :rocket: update apache licenses

--- src/main/java/com/mobaijun/common/util/pinyin/package-info.java
+++ src/main/java/com/mobaijun/common/util/pinyin/package-info.java
@@ -13,38 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.mobaijun.common.enums.common;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 /**
- * software：IntelliJ IDEA 2022.2.3<br>
- * enum name: JavaDocEnum<br>
- * enum description: Java 注释常量<br>
+ * software：IntelliJ IDEA 2022.2.3
+ * package name: com.mobaijun.common.util.pinyin
+ * package description：拼音
  *
- * @author MoBaiJun 2022/12/9 15:23
+ * @author MoBaiJun 2022/10/27 9:02
  */
-@Getter
-@AllArgsConstructor
-public enum JavaDocEnum {
-
-    /**
-     * 注释前缀
-     */
-    COMMENT_PREFIX("/**", "注释前缀"),
-
-    /**
-     * 注释后缀
-     */
-    COMMENT_SUFFIX("*/", "注释后缀"),
-
-    /**
-     * 换行
-     */
-    COMMENT_RETURN("\n", "换行");
-
-    private final String name;
-
-    private final String desc;
-}
+package com.mobaijun.common.util.pinyin;
