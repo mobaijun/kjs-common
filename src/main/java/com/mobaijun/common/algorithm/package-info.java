32363228b0557cd039a2eee4e8c5a8a7bb456572
mobaijun <mobaijun8@163.com>
2023-02-20 08:58:01 +0800

    :sparkles: 添加算法包说明|更新 apache 版权协议

--- src/main/java/com/mobaijun/common/algorithm/package-info.java
+++ src/main/java/com/mobaijun/common/algorithm/package-info.java
@@ -18,6 +18,9 @@
  * package name: com.mobaijun.common.algorithm
  * package description： 算法包
  * <p>
+ * 其中，冒泡排序、选择排序、插入排序都是比较简单直观的算法，但时间复杂度较高，通常用于小规模数据排序。
+ * 快速排序、归并排序、堆排序等算法的时间复杂度较低，适用于大规模数据排序。
+ * 而桶排序、计数排序、基数排序等算法则是特殊场景下的高效排序算法，适用于某些数据范围较小的场景。
  * 感谢：
  * https://www.cnblogs.com/guoyaohua/p/8600214.html
  * https://chat.openai.com/chat/
