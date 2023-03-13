949de40c37cf1da20acf491367043399ff9203d3
mobaijun <mobaijun8@163.com>
2023-03-13 11:21:10 +0800

    :sparkles: 日期格式添加说明

--- src/main/java/com/mobaijun/common/util/date/DatePattern.java
+++ src/main/java/com/mobaijun/common/util/date/DatePattern.java
@@ -24,77 +24,193 @@ package com.mobaijun.common.util.date;
  */
 public final class DatePattern {
 
+    /**
+     * 日期格式：uuuu-MM-dd HH:mm:ss
+     */
     public static final String UUUU_MM_DD_HH_MM_SS = "uuuu-MM-dd HH:mm:ss";
 
+    /**
+     * 日期格式：uuuu/MM/dd HH:mm:ss
+     */
     public static final String UUUU_MM_DD_SLASH_HH_MM_SS = "uuuu/MM/dd HH:mm:ss";
 
+    /**
+     * 日期格式：uuuu.MM.dd HH:mm:ss
+     */
     public static final String UUUU_MM_DD_DOT_HH_MM_SS = "uuuu.MM.dd HH:mm:ss";
 
+    /**
+     * 日期格式：uuuuMMddHHmmss
+     */
     public static final String UUUUMMDDHHMMSS = "uuuuMMddHHmmss";
 
+    /**
+     * 日期格式：uuuu-MM-dd HH:mm
+     */
     public static final String UUUU_MM_DD_HH_MM = "uuuu-MM-dd HH:mm";
 
+    /**
+     * 日期格式：uuuu/MM/dd HH:mm
+     */
     public static final String UUUU_MM_DD_SLASH_HH_MM = "uuuu/MM/dd HH:mm";
 
+    /**
+     * 日期格式：uuuu.MM.dd HH:mm
+     */
     public static final String UUUU_MM_DD_DOT_HH_MM = "uuuu.MM.dd HH:mm";
 
+    /**
+     * 日期格式：uuuu-MM-dd
+     */
     public static final String UUUU_MM_DD = "uuuu-MM-dd";
 
+    /**
+     * 日期格式：uuuu/MM/dd
+     */
     public static final String UUUU_MM_DD_SLASH = "uuuu/MM/dd";
 
+    /**
+     * 日期格式：uuuu.MM.dd
+     */
     public static final String UUUU_MM_DD_DOT = "uuuu.MM.dd";
 
+    /**
+     * 日期格式：uuuu-MM
+     */
     public static final String UUUU_MM = "uuuu-MM";
 
+    /**
+     * 日期格式：uuuu/MM
+     */
     public static final String UUUU_MM_SLASH = "uuuu/MM";
 
+    /**
+     * 日期格式：uuuu.MM
+     */
     public static final String UUUU_MM_DOT = "uuuu.MM";
 
+    /**
+     * 日期格式：yyyy-MM-dd HH:mm:ss
+     */
     public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
+    /**
+     * 日期格式：yyyy/MM/dd HH:mm:ss
+     */
     public static final String YYYY_MM_DD_SLASH_HH_MM_SS = "yyyy/MM/dd HH:mm:ss";
 
+    /**
+     * 日期格式：yyyy.MM.dd HH:mm:ss
+     */
     public static final String YYYY_MM_DD_DOT_HH_MM_SS = "yyyy.MM.dd HH:mm:ss";
 
+    /**
+     * 日期格式：yyyyMMddHHmmss
+     */
     public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
 
+    /**
+     * 日期格式：yyyy-MM-dd HH:mm
+     */
     public static final String YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
 
+    /**
+     * 日期格式：yyyy/MM/dd HH:mm
+     */
     public static final String YYYY_MM_DD_SLASH_HH_MM = "yyyy/MM/dd HH:mm";
 
+    /**
+     * 日期格式：yyyy.MM.dd HH:mm
+     */
     public static final String YYYY_MM_DD_DOT_HH_MM = "yyyy.MM.dd HH:mm";
+
+    /**
+     * 按照 yyyy-MM-dd 格式的日期字符串常量
+     */
     public static final String YYYY_MM_DD = "yyyy-MM-dd";
 
+    /**
+     * 按照 yyyy/MM/dd 格式的日期字符串常量
+     */
     public static final String YYYY_MM_DD_SLASH = "yyyy/MM/dd";
 
+    /**
+     * 按照 yyyy.MM.dd 格式的日期字符串常量
+     */
     public static final String YYYY_MM_DD_DOT = "yyyy.MM.dd";
 
+    /**
+     * 按照 yyyy-MM 格式的日期字符串常量
+     */
     public static final String YYYY_MM = "yyyy-MM";
 
+    /**
+     * 按照 yyyy/MM 格式的日期字符串常量
+     */
     public static final String YYYY_MM_SLASH = "yyyy/MM";
 
+    /**
+     * 按照 yyyy.MM 格式的日期字符串常量
+     */
     public static final String YYYY_MM_DOT = "yyyy.MM";
 
+    /**
+     * 按照 MM-dd HH:mm:ss 格式的日期字符串常量
+     */
     public static final String MM_DD_HH_MM_SS = "MM-dd HH:mm:ss";
 
+    /**
+     * 按照 MM/dd HH:mm:ss 格式的日期字符串常量
+     */
     public static final String MM_DD_SLASH_HH_MM_SS = "MM/dd HH:mm:ss";
 
+    /**
+     * 按照 MM.dd HH:mm:ss 格式的日期字符串常量
+     */
     public static final String MM_DD_DOT_HH_MM_SS = "MM.dd HH:mm:ss";
 
+    /**
+     * 按照 MMddHHmmss 格式的日期字符串常量
+     */
     public static final String MMDDHHMMSS = "MMddHHmmss";
 
+    /**
+     * 按照 MM-dd HH:mm 格式的日期字符串常量
+     */
     public static final String MM_DD_HH_MM = "MM-dd HH:mm";
 
+    /**
+     * 按照 MM/dd HH:mm 格式的日期字符串常量
+     */
     public static final String MM_DD_SLASH_HH_MM = "MM/dd HH:mm";
 
+    /**
+     * 按照 MM.dd HH:mm 格式的日期字符串常量
+     */
     public static final String MM_DD_DOT_HH_MM = "MM.dd HH:mm";
 
+    /**
+     * 按照 MM-dd 格式的日期字符串常量
+     */
     public static final String MM_DD = "MM-dd";
 
+    /**
+     * 按照 MM/dd 格式的日期字符串常量
+     */
     public static final String MM_DD_SLASH = "MM/dd";
 
+    /**
+     * 按照 MM.dd 格式的日期字符串常量
+     */
     public static final String MM_DD_DOT = "MM.dd";
+
+    /**
+     * 按照 HH:mm:ss 格式的时间字符串常量
+     */
     public static final String HH_MM_SS = "HH:mm:ss";
 
+    /**
+     * 按照 HH:mm 格式的时间字符串常量
+     */
     public static final String HH_MM = "HH:mm";
 }
